f632e2aa4538e2f2b0c1d2ed8853138c76932632
DesarrolloWeb23 <Desarrollo1@pijaossalud.com.co>
2025-10-15 07:44:46 -0500

    fix
    Se agregan variables de entorno para el cliente de contentful

--- src/core/infrastructure/api/http/client.ts
+++ src/core/infrastructure/api/http/client.ts
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-    space: 'br05rbdgaer4',
-    accessToken: '0D_zTF9Srcphy5pUmrWXU127rv_6OExPWqXqSnaQaJA',
+    space: process.env.CLIENT_SPACE_ID || '',
+    accessToken: process.env.CLIENT_ACCESS_TOKEN || '',
 });
