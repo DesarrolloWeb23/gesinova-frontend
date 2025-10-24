a694dbad39028b17760cb4942fd303e8ce384800
DesarrolloWeb23 <Desarrollo1@pijaossalud.com.co>
2025-10-24 10:53:17 -0500

    fix

--- src/core/infrastructure/api/http/client.ts
+++ src/core/infrastructure/api/http/client.ts
@@ -1,6 +1,13 @@
 import * as contentful from 'contentful';
 
+const space = process.env.NEXT_PUBLIC_CLIENT_SPACE_ID;
+const accessToken = process.env.NEXT_PUBLIC_CLIENT_ACCESS_TOKEN;
+
+if (!space || !accessToken) {
+    throw new Error("❌ Faltan variables de entorno de Contentful");
+}
+
 export const client = contentful.createClient({
-    space: process.env.NEXT_PUBLIC_CLIENT_SPACE_ID  || '',
-    accessToken: process.env.NEXT_PUBLIC_CLIENT_ACCESS_TOKEN  || '',
+    space,
+    accessToken,
 });
