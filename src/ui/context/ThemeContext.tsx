77c1c123b27d93124427de8c12f2cecb5bd76770
DesarrolloWeb23 <Desarrollo1@pijaossalud.com.co>
2025-10-29 13:06:46 -0500

    style: halloween
    Se realiza incorporacion de estilos para poner tema de halloween

--- src/ui/context/ThemeContext.tsx
+++ src/ui/context/ThemeContext.tsx
@@ -7,5 +7,51 @@ export function ThemeProvider({
     children,
     ...props
     }: React.ComponentProps<typeof NextThemesProvider>) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    return (
+        <NextThemesProvider {...props}>
+            <div className="snowflakes">
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="60" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+                <div className="snowflake">
+                    <img src="https://i.pinimg.com/originals/d1/96/a9/d196a94b9d8cda6edfd3a65237aa8fb5.gif" 
+                        width="50" 
+                        alt="155b4588f223ce388f2542198570256b.gif" />
+                </div>
+            </div>
+            {children}
+        </NextThemesProvider>
+    )
 }
